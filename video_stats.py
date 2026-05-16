767494bfab38a2c80dd58343fc7287dec9902b85
charanteja chikkudu <charanteja.chikkudu@personifyhealth.com>
2026-05-16 16:25:09 +0530

    save data in json file in /data

--- video_stats.py
+++ video_stats.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os 
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="./.env")
 API_KEY = os.getenv("API_KEY")
@@ -98,9 +99,16 @@ def extract_video_data(video_ids):
     except requests.exceptions.RequestException as e:
         raise e
 
+def save_to_json(extacted_data):
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extacted_data, json_outfile, indent=4, ensure_ascii=False)
+
 if __name__ == "__main__":
     playlistId = get_playlist_id()
     video_ids = get_video_ids(playlistId)
-    print(extract_video_data(video_ids=video_ids))
+    video_data = extract_video_data(video_ids=video_ids)
+    save_to_json(video_data)
 else:
     print("get_playlist_id won't be executed")
