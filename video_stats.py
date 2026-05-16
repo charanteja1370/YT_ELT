7a352f69d5b6b68b1a3d11c0747fe924ea212732
charanteja chikkudu <charanteja.chikkudu@personifyhealth.com>
2026-05-16 16:06:24 +0530

    videos data

--- video_stats.py
+++ video_stats.py
@@ -60,9 +60,47 @@ def get_video_ids(playlistID):
     except requests.exceptions.RequestException as e:
         raise e
 
+def batch_list(video_id_lst, batch_size):
+    for video_id in range(0, len(video_id_lst), batch_size):
+        yield video_id_lst[video_id:video_id + batch_size]
+
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_lst, batch_size):
+        for video_id in range(0, len(video_id_lst), batch_size):
+            yield video_id_lst[video_id:video_id + batch_size]
+        
+        "https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id=0e3GPea1Tyg&key=[YOUR_API_KEY]"
+
+    try:
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",".join(batch)
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data["items"]:
+                video_data = {
+                    "videoId": item["id"],
+                    "title": item["snippet"]["title"],
+                    "publishedAt": item["snippet"]["publishedAt"],
+                    "duration": item["contentDetails"]["duration"],
+                    "viewCount": item["statistics"].get("viewCount", None),  # viewCount may not be available
+                    "likeCount": item["statistics"].get("likeCount", None),  # likeCount may not be available
+                    "commentCount": item["statistics"].get("commentCount", None)  # commentCount may not be available
+                }
+                extracted_data.append(video_data)
+
+        return extracted_data
+    except requests.exceptions.RequestException as e:
+        raise e
 
 if __name__ == "__main__":
     playlistId = get_playlist_id()
-    print(get_video_ids(playlistId))
+    video_ids = get_video_ids(playlistId)
+    print(extract_video_data(video_ids=video_ids))
 else:
     print("get_playlist_id won't be executed")
